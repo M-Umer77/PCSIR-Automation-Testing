f65d3b2538021c37a3bbe07fd6f141ea3bf656ee
M-Umer77 <159107635+M-Umer77@users.noreply.github.com>
2026-05-14 12:47:05 +0500

    New Changes

--- playwright.config.js
+++ playwright.config.js
@@ -3,19 +3,25 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   timeout: 30000,
+
   expect: {
     timeout: 10000,
   },
+
   use: {
     baseURL: process.env.BASE_URL || "http://10.3.3.86:4008",
     headless: false,
-    viewport: {
-      width: 1366,
-      height: 768,
+
+    // Browser ko full screen/maximized jaisa open karega
+    viewport: null,
+    launchOptions: {
+      args: ["--start-maximized"],
     },
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
-    trace: "retain-on-failure",
+
+    screenshot: "on",
+    video: "on",
+    trace: "on",
   },
-  reporter: [["list"], ["html", { open: "never" }]],
+
+  reporter: "list",
 });
